a7a56d5875bbe2ba40c52361dbbe20747df49ced
H. Watanabe <kaityo@users.sourceforge.jp>
2020-04-19 11:03:59 +0900

    modified density

--- generate_config.py
+++ generate_config.py
@@ -11,7 +11,7 @@ class Atom:
 
 def add_ball(atoms, xpos, xvel):
     r = 8
-    s = 1.93
+    s = 1.55
     h = 0.5 * s
     for ix in range(-r, r+1):
         for iy in range(-r, r+1):
@@ -22,7 +22,7 @@ def add_ball(atoms, xpos, xvel):
                 if (x**2 + y**2 + z**2 > r**2):
                     continue
                 x = x + xpos
-                atoms.append(Atom(x, y, i, xvel))
+                atoms.append(Atom(x, y, z, xvel))
                 atoms.append(Atom(x, y+h, z+h, xvel))
                 atoms.append(Atom(x+h, y, z+h, xvel))
                 atoms.append(Atom(x+h, y+h, z, xvel))
