4107556b18ba961b4c711afa415309128d5a37ef
H. Watanabe <kaityo@users.sourceforge.jp>
2025-04-28 11:34:35 +0900

    :memo: udpates

--- generate_config.py
+++ generate_config.py
@@ -13,19 +13,19 @@ def add_ball(atoms, xpos, xvel):
     r = 8
     s = 1.55
     h = 0.5 * s
-    for ix in range(-r, r+1):
-        for iy in range(-r, r+1):
-            for iz in range(-r, r+1):
+    for ix in range(-r, r + 1):
+        for iy in range(-r, r + 1):
+            for iz in range(-r, r + 1):
                 x = ix * s
                 y = iy * s
                 z = iz * s
-                if (x**2 + y**2 + z**2 > r**2):
+                if x**2 + y**2 + z**2 > r**2:
                     continue
                 x = x + xpos
                 atoms.append(Atom(x, y, z, xvel))
-                atoms.append(Atom(x, y+h, z+h, xvel))
-                atoms.append(Atom(x+h, y, z+h, xvel))
-                atoms.append(Atom(x+h, y+h, z, xvel))
+                atoms.append(Atom(x, y + h, z + h, xvel))
+                atoms.append(Atom(x + h, y, z + h, xvel))
+                atoms.append(Atom(x + h, y + h, z, xvel))
 
 
 def save_file(filename, atoms):
@@ -49,7 +49,7 @@ def save_file(filename, atoms):
 
 atoms = []
 
-add_ball(atoms, -20, 2.0)
-add_ball(atoms, 20, -2.0)
+add_ball(atoms, -20, 1.0)
+add_ball(atoms, 20, -1.0)
 
 save_file("collision.atoms", atoms)
